15f028dd93479ac2dc37a25e694d10f27172f20b
Brian Scherner <brianscherner@gmail.com>
2023-07-11 11:59:19 -0700

    Write calculateCost method for pizza constructor.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,24 @@
-// function Pizza() {
-// 	this.toppings = "";
-// 	this.size = 0;
-// }
-
 function Pizza(toppings, size) {
-	this.toppings = ["pepperoni", "sausage", "pineapple"];
-	this.size = "medium";
+	this.toppings = toppings;
+	this.size = size;
+	this.cost = 0;
 }
 
-let pizza = new Pizza();
+Pizza.prototype.calculateCost = function() {
+	if (this.size === "small") {
+		this.cost += 10;
+	} else if (this.size === "medium") {
+		this.cost += 12;
+	} else if (this.size === "large") {
+		this.cost += 14;
+	}
+	return this.cost;
+}
+
+const pizza = new Pizza(["pepperoni", "sausage", "pineapple"], "medium");
+// let pizza = new Pizza();
 
 console.log(pizza);
+pizza.calculateCost();
+console.log(pizza);
+
